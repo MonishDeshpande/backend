45fd0c2ef4c98e3ab2697034935b1dfe8faf32f0
MonishDeshpande <monish10102907@gmail.com>
2024-08-21 09:49:56 +0530

    Added User Schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const serverConfig = require("./config/serverConfig.js");
 const connectDB = require("./config/dbConfig.js");
 
 const PORT = serverConfig.PORT; // SRP -> Single Responsibility Principle
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
+
 
 app.listen(PORT, async () => {
   await connectDB();
---
+++ src/schemas/userSchema.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+      minlength: [5, "First name must be at least 5 characters long"],
+      lowercase: true,
+      trim: true,
+      maxlength: [20, "First name must be at most 20 characters long"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+      minlength: [5, "Last name must be at least 5 characters long"],
+      lowercase: true,
+      trim: true,
+      maxlength: [20, "Last name must be at most 20 characters long"],
+    },
+    mobileNumber: {
+      type: String,
+      required: [true, "Mobile number is required"],
+      unique: true,
+      match: [/^(\+\d{1,3}[- ]?)?\d{10}$/, "Invalid mobile number"],
+      trim: true,
+      maxlength: [10, "Mobile number must be 10 digits long"],
+      minlength: [10, "Mobile number must be 10 digits long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid email"],
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters long"],
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
